2f6a369505cac75721e0f19b13ea318845af4196
Dmytro Nazarenko <dmamyk@gmail.com>
2019-07-29 15:04:33 +0300

    Updates

--- source/API/config.js
+++
@@ -1,6 +0,0 @@
-const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com';
-const ROOT_URL = `${CORS_ANYWHERE}/https://api.yelp.com`;
-const API_KEY = 'LmcyFUDhaHET3FZnhdSPjfaYXFhF7cztuX7zzXy84RTycyXVPMIfuFYmuUs-pQ_cyXUMgc0zS1_J20aSwgxo0xr7aIWC2qRiykLQ4fv8n8CvRXGqwvZkbJfbxKk-XXYx';
-
-export { ROOT_URL, API_KEY };
-
--- source/API/index.js
+++
@@ -1,2 +0,0 @@
-export { api } from './REST';
-export { ROOT_URL, API_KEY } from './config';
